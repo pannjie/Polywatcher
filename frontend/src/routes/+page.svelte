c5a6e96c07b14960cbf28cb53856a5d71701ef99
pannjie <98586016+pannjie@users.noreply.github.com>
2026-03-17 01:22:57 +0000

    UI update

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -15,12 +15,16 @@
   <p class="label">Developed by PJ/Goldsmiths</p>
 </fieldset>
 
-<div class="flex flex-wrap justify-center gap-2 mt-8">
-  <a class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x44de2a52d8d2d3ddcf39d58e315a10df53ba9c08">Maduro Whale</a>
-  <a class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x94a428cfa4f84b264e01f70d93d02bc96cb36356">George Cottrell</a>
-  <a class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x4dfd481c16d9995b809780fd8a9808e8689f6e4a">Iran Whale #1</a>
-  <a class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x162f6fff88a52864f2ecc9833e58089d5254798d">Superbowl Insider</a>
-  <a class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0xbad457dc633bbb7b6cbe09dd5867a5e8e597acd7">Iran Whale #2</a>
+<div class="flex flex-col items-center gap-2 mt-8">
+  <div class="flex gap-2">
+    <a class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x44de2a52d8d2d3ddcf39d58e315a10df53ba9c08">Maduro Whale</a>
+    <a class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x94a428cfa4f84b264e01f70d93d02bc96cb36356">George Cottrell</a>
+    <a class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x4dfd481c16d9995b809780fd8a9808e8689f6e4a">Iran Whale #1</a>
+  </div>
+  <div class="flex gap-2">
+    <a class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x162f6fff88a52864f2ecc9833e58089d5254798d">Superbowl Insider</a>
+    <a class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0xbad457dc633bbb7b6cbe09dd5867a5e8e597acd7">Iran Whale #2</a>
+  </div>
 </div>
 </div>
 
