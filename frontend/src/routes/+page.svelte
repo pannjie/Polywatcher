f72601558ad13a62ad437c39ee69626b46d23471
pannjie <98586016+pannjie@users.noreply.github.com>
2026-03-17 00:55:06 +0000

    UI updates

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -16,6 +16,14 @@
 </fieldset>
 </div>
 
+<div class="flex flex-wrap justify-center gap-2 mt-2">
+  <button class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x44de2a52d8d2d3ddcf39d58e315a10df53ba9c08">Maduro Whale</button>
+  <button class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x94a428cfa4f84b264e01f70d93d02bc96cb36356">George Cottrell</button>
+  <button class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x4dfd481c16d9995b809780fd8a9808e8689f6e4a">Iran Whale #1</button>
+  <button class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0x162f6fff88a52864f2ecc9833e58089d5254798d">Superbowl Insider</button>
+  <button class="btn btn-outline btn-sm" href="https://polywatcherfrontend-production.up.railway.app/profile/0xbad457dc633bbb7b6cbe09dd5867a5e8e597acd7">Iran Whale #2</button>
+</div>
+
 <style>
   
 </style>
